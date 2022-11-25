0682bb1f8876ddf9e31135c969c9218de5a3b963
Sushvin MariMuthu <19csa50@karpagamtech.ac.in>
2022-11-25 17:35:31 +0530

    Deploy

---
+++ Project Development Phase/Sprint 4/static/js/company_index.js
@@ -0,0 +1,12 @@
+$(document).on('submit','#job_creation_form',function(e){
+    e.preventDefault();
+    const job_creation_form = $(this).serialize();
+    $.ajax({
+        type: 'POST',
+        url: '/create_job',
+        data: job_creation_form,
+        success: function (response) {
+            alert(response);
+        },
+    });
+});
